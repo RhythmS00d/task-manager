f7527ce0f095103c4c748807f066cde406679121
RhythmS00d <rhythmsood1@gmail.com>
2024-03-04 11:17:36 +1100

    added quote widget to home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,11 @@ import { WidgetCard } from "@/components/WidgetCard";
 
 export default function Home() {
   return (
-    <section className="flex gap-10 p-4">
-      <WidgetCard name="Weather" imageUrl="/weather.png"/>
-      <WidgetCard name="Task-Manager" imageUrl="/task-manager.png"/>
-      <WidgetCard name="News" imageUrl="/news.png"/>
+    <section className="flex gap-10 p-4 flex-wrap items-center justify-center">
+      <WidgetCard name="Weather" imageUrl="/weather.png" />
+      <WidgetCard name="Task-Manager" imageUrl="/task-manager.png" />
+      <WidgetCard name="News" imageUrl="/news.png" />
+      <WidgetCard name="Quote-Generator" imageUrl="/quote.png" />
     </section>
   );
 }
